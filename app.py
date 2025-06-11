3c95b6e702c633364eca77addd81eddd5fb2534c
Fauzi Aditya <fauziaditya874@jmail.com>
2025-06-11 15:37:19 +0700

    Add main program

--- app.py
+++ app.py
@@ -539,3 +539,4 @@ with tab4: model_keandalan_produksi()
 # --- FOOTER ---
 st.divider()
 st.caption("© 2025 Kelompok 9 - Matematika Terapan | Dikembangkan untuk Tugas Kelompok")
+st.caption("Fauzi Aditya | Marita Andika Putri | Naufal Khoirul Ibrahim | Poppi Marsanti Ramadanin")
